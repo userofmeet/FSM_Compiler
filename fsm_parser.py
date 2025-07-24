e64b515acf299e8b6304419d2a13443a69159e87
Meet Jain <meetjain538@gmail.com>
2025-07-24 10:01:19 +0530

    Update fsm_parser.py

--- fsm_parser.py
+++ fsm_parser.py
@@ -1,7 +1,7 @@
 # format with fsm: states: events: and transitions: sections
 def parse_fsm_file(file_path):
-    drone_systems = []
-    current_drone = {}
+    systems = []
+    current = {}
     parsing_mode = None
 
     with open(file_path, 'r') as file:
@@ -12,9 +12,9 @@ def parse_fsm_file(file_path):
         if not line or line.startswith("#"):  # skip empty files
             continue
         if line.startswith("fsm:"):
-            if current_drone:  # save previous fsms
-                drone_systems.append(current_drone)
-            current_drone = {
+            if current:  # save previous fsms
+                systems.append(current)
+            current = {
                 "name": line.replace("fsm:", "").strip(),
                 "states": [],
                 "events": [],
@@ -22,9 +22,9 @@ def parse_fsm_file(file_path):
             }
             parsing_mode = None
         elif line.startswith("states:"):
-            current_drone["states"] = [state.strip() for state in line.replace("states:", "").split(",")]
+            current["states"] = [state.strip() for state in line.replace("states:", "").split(",")]
         elif line.startswith("events:"):
-            current_drone["events"] = [event.strip() for event in line.replace("events:", "").split(",")]
+            current["events"] = [event.strip() for event in line.replace("events:", "").split(",")]
         elif line.startswith("transitions:"):
             parsing_mode = "transitions"
         elif parsing_mode == "transitions":
@@ -32,9 +32,9 @@ def parse_fsm_file(file_path):
             transition = parts[0].strip()
             event = parts[1].strip()
             source, destination = [state.strip() for state in transition.split("->")]
-            current_drone["transitions"].append((source, event, destination))
+            current["transitions"].append((source, event, destination))
 
-    if current_drone:  # last fms
-        drone_systems.append(current_drone)
+    if current:  # last fms
+        systems.append(current)
 
-    return drone_systems
+    return systems
